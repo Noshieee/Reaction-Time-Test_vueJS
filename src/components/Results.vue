1727d9b2ce11f456b1576b41203aa8851b67dd44
Enosh Elliott <enoshelliott14@gmail.com>
2022-01-25 12:03:00 +0200

    finished reaction test

---
+++ src/components/Results.vue
@@ -0,0 +1,31 @@
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data() {
+        return {
+            rank:null
+        }
+    },
+    mounted() {
+        if (this.score < 290) {
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail Pace...'
+        }
+    },
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4rem;
+        color: purple;
+        font-weight: bold;
+    }
+</style>
